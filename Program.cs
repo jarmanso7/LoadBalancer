f5fa8b6f34a7ea9f3e5e695a3253e3d87190aa31
Jordi Armengue Soler <armenguj@emea.roche.com>
2020-04-18 19:29:45 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadBalancer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var loads = new List<int[]>();
+            loads.Add(new int[] { 1, 1, 1, 1 });
+            loads.Add(new int[] { 1, 1, 1, 1, 1 });
+            loads.Add(new int[] { 1, 2, 3, 4, 5, 6 });
+            loads.Add(new int[] { 1, 2, 9, 3, 5, 1, 1, 1 });
+            loads.Add(new int[] { 2, 4, 5, 3, 3, 9, 2, 2, 2});
+
+            //  Expected output:
+            //      false
+            //      true
+            //      false
+            //      true
+            //      true
+
+            foreach (var load in loads)
+            {
+                Console.WriteLine(LoadBalancer(load).ToString());
+            }
+
+            Console.ReadKey();
+        }
+
+        static bool LoadBalancer(int[] numbers)
+        {
+            var r = 0;
+            var t = 1;
+            int f;
+
+            while (r < numbers.Length - 1)
+            {
+                f = t;
+                while(f < numbers.Length)
+                {
+                    if (IsBalancedForPAndQPositions(numbers, r, f))
+                    {
+                        return true;
+                    }
+                    f++;
+                }
+
+                r++;
+                t++;
+            }
+            return false;
+        }
+
+        static bool IsBalancedForPAndQPositions(int[] numbers, int p, int q)
+        {
+            if (p == 0 || q == numbers.Length - 1 || q - p == 1)
+                return false;
+
+            var firstSum = SumElements(numbers, 0, p - 1);
+            var secondSum = SumElements(numbers, p + 1, q - 1);
+            var thirdSum = SumElements(numbers, q + 1, numbers.Length - 1);
+
+            return firstSum == secondSum && firstSum == thirdSum;
+        }
+
+        static int SumElements(int[] numbers, int startIndex, int endIndex)
+        {
+            var sum = 0;
+            for (var i = startIndex; i <= endIndex; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# Load Balancer
+## What it is
+Implementation a solution to the [Load Balancer problem](https://aonecode.com/amazon-online-assessment-load-balancer) in C#:
+
+## Explanation
+Given an array containing only positive integers, return if you can pick two integers from the array which cuts the array into three pieces such that the sum of elements in all pieces is equal.
+
+## Example
+
+### Case 1
+
+Input:  array = [2, 4, 5, 3, 3, 9, 2, 2, 2]
+
+Output: true
+
+choosing the number 5 and 9 results in three pieces [2, 4], [3, 3] and [2, 2, 2]. Sum = 6.
+
+### Case 2
+
+Input:  array =[1, 1, 1, 1],
+
+Output: false
